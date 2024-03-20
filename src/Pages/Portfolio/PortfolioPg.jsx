166ae96ea74b0af4d2fa320e5f4622a3b656bc4d
Naiabunner <Okechukwudaniel12@gmail.com>
2024-03-20 04:59:14 +0100

    redirects update

--- src/Pages/Portfolio/PortfolioPg.jsx
+++ src/Pages/Portfolio/PortfolioPg.jsx
@@ -10,7 +10,7 @@ import 'swiper/css/navigation';
 const PortfolioPage = () => {
     return (
       <>
-        <nav className="absolute left-5 top-5 flex justify-between bg-none w-[90%]">
+        <nav className="absolute left-5 top-5 flex justify-between items-center bg-none w-[90%]">
           <a href="/">
             {" "}
             <h1 className=" text-5xl font-bold">JO</h1>
@@ -28,7 +28,9 @@ const PortfolioPage = () => {
               <p className="mx-5">Contact Me</p>
             </a>
           </div>
-          <p className="block md:hidden"> bars</p>
+          <p className="block md:hidden"> 
+          <svg xmlns="http://www.w3.org/2000/svg" className=' w-7' viewBox="0 0 512 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM64 256c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H96c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+          </p>
         </nav>
         
         <main id="main" className=" flex  mt-16 overflow-hidden">
