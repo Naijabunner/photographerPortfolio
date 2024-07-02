355a7ca97cca61755c3f1c77fc3293dcb63cbbad
Burna <Okechukwudaniel12@gmail.com>
2024-07-02 08:07:47 +0100

    just push abeg

--- src/Pages/Portfolio/PortfolioPg.jsx
+++ src/Pages/Portfolio/PortfolioPg.jsx
@@ -3,6 +3,29 @@ import { motion, AnimatePresence } from "framer-motion";
 import Navbar from "../../Nav";
 import GalleryShimmer from "../../components/gallerySkeleton";
 import LazyImage from "../../components/lazyloader";
+import { ImageArr } from "../../Lib/Images";
+const image0 = React.lazy(() => import('../../../public/gallery/IMG(22).jpg'));
+const image1 = React.lazy(() => import('../../../public/gallery/IMG(23).jpg'));
+const image2 = React.lazy(() => import('../../../public/gallery/IMG(24).jpg'));
+const image3 = React.lazy(() => import('../../../public/gallery/IMG(25).jpg'));
+const image4 = React.lazy(() => import('../../../public/gallery/IMG(26).jpg'));
+const image5 = React.lazy(() => import('../../../public/gallery/IMG(27).jpg'));
+const image6 = React.lazy(() => import('../../../public/gallery/IMG(28).jpg'));
+const image7 = React.lazy(() => import('../../../public/gallery/IMG(29).jpg'));
+const image8 = React.lazy(() => import('../../../public/gallery/IMG(30).jpg'));
+const image9 = React.lazy(() => import('../../../public/gallery/IMG(31).jpg'));
+const image10 = React.lazy(() => import('../../../public/gallery/IMG(32).jpg'));
+const image11 = React.lazy(() => import('../../../public/gallery/IMG(33).jpg'));
+const image12 = React.lazy(() => import('../../../public/gallery/IMG(34).jpg'));
+const image13 = React.lazy(() => import('../../../public/gallery/IMG(35).jpg'));
+const image14 = React.lazy(() => import('../../../public/gallery/IMG(36).jpg'));
+const image15 = React.lazy(() => import('../../../public/gallery/IMG(37).jpg'));
+const image16 = React.lazy(() => import('../../../public/gallery/IMG(38).jpg'));
+const image17 = React.lazy(() => import('../../../public/gallery/IMG(39).jpg'));
+const image18 = React.lazy(() => import('../../../public/gallery/IMG(40).jpg'));
+const image19 = React.lazy(() => import('../../../public/gallery/IMG(41).jpg'));
+const image20 = React.lazy(() => import('../../../public/gallery/IMG(42).jpg'));
+const image21 = React.lazy(() => import('../../../public/gallery/IMG(43).jpg'));
 
 const PortfolioPage = () => {
   const [theme, setTheme] = useState("light");
@@ -33,7 +56,7 @@ const PortfolioPage = () => {
           transition={{
             staggerChildren: 0.05,
           }}
-          className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-5 "
+          className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-5 "
         >
 <ImagesContainer/>
         </motion.div>
@@ -65,7 +88,7 @@ const ImagesContainer=()=>{
         {entries[0]?.data?.map((ent, innerIndex) => {
           return (
             <motion.div key={innerIndex}
-              
+              className="rounded-lg bg-gray-300 bg-opacity-10"
               variants={{
                 initial: {
                   scale: 0.5,
@@ -92,50 +115,209 @@ const ImagesContainer=()=>{
 }</>
   )
 }
+
+
+
+
+// const images =
+// [
+  
+//    [{data:[ {
+//     src:'../../../public/gallery/IMG(22).jpg',
+//    alt:""
+//     },
+//    {
+//    src:'../../../public/gallery/IMG(23).jpg',
+//    alt:""
+//     },
+//    {
+//    src:'../../../public/gallery/IMG(24).jpg',
+//    alt:""
+//     },
+    
+//   ]}]
+//   ,
+//   [{data:
+//    [{
+//     src:'../../../public/gallery/IMG(25).jpg',
+//     alt:""
+//      },
+//     {
+//     src:'../../../public/gallery/IMG(26).jpg',
+//     alt:""
+//      },
+//     {
+//     src:'../../../public/gallery/IMG(27).jpg',
+//     alt:""
+//      },] }]
+//   ,
+//   [{data:
+//    [ {
+//    src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
+//    alt:""
+//     },
+//     {
+//    src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
+//    alt:""
+//     },
+//     {
+//    src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
+//    alt:""
+//     },] }]
+//   ,
+//   [{data:
+//    [ {
+//    src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
+//    alt:""
+//     },
+//     {
+//    src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
+//    alt:""
+//     },
+//     {
+//    src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
+//    alt:""
+//     },] }]
+//   ,
+//   [{data:
+//    [ {
+//    src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
+//    alt:""
+//     },
+//     {
+//    src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
+//    alt:""
+//     },
+//     {
+//    src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
+//    alt:""
+//     },] }]
+//   ,
+ 
+// ]
 const images =
 [
-  
-   [{data:[ {
-   src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
-   alt:""
-    },
+  [{data:
+   [ 
     {
-   src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
-   alt:""
-    },
-    {
-   src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
-   alt:""
-    }]}]
+           src:'../../../public/gallery/IMG(22).jpg',
+           alt:""
+            },
+           {
+           src:'../../../public/gallery/IMG(23).jpg',
+           alt:""
+            },
+           {
+           src:'../../../public/gallery/IMG(24).jpg',
+           alt:""
+            }
+    ,] }]
   ,
   [{data:
-   [ {
-   src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
-   alt:""
-    },
+   [ 
     {
-   src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
-   alt:""
-    },
-    {
-   src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
-   alt:""
-    },] }]
+           src:'../../../public/gallery/IMG(25).jpg',
+           alt:""
+            },
+           {
+           src:'../../../public/gallery/IMG(26).jpg',
+           alt:""
+            },
+           {
+           src:'../../../public/gallery/IMG(27).jpg',
+           alt:""
+            }
+    ,] }]
   ,
   [{data:
-   [ {
-   src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
-   alt:""
-    },
+   [ 
     {
-   src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
-   alt:""
-    },
-    {
-   src:"https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
-   alt:""
-    },] }]
+           src:'../../../public/gallery/IMG(28).jpg',
+           alt:""
+            },
+           {
+           src:'../../../public/gallery/IMG(29).jpg',
+           alt:""
+            },
+           {
+           src:'../../../public/gallery/IMG(30).jpg',
+           alt:""
+            }
+    ,] }]
   ,
+  [{data:
+   [ 
+    {
+           src:'../../../public/gallery/IMG(31).jpg',
+           alt:""
+            },
+           {
+           src:'../../../public/gallery/IMG(32).jpg',
+           alt:""
+            },
+           {
+           src:'../../../public/gallery/IMG(33).jpg',
+           alt:""
+            }
+    ,] }]
+  ,
+  [{data:
+   [ 
+    {
+           src:'../../../public/gallery/IMG(34).jpg',
+           alt:""
+            },
+           {
+           src:'../../../public/gallery/IMG(35).jpg',
+           alt:""
+            },
+           {
+           src:'../../../public/gallery/IMG(36).jpg',
+           alt:""
+            }
+    ,] }]
+  ,
+  [{data:
+   [ 
+    {
+           src:'../../../public/gallery/IMG(37).jpg',
+           alt:""
+            },
+           {
+           src:'../../../public/gallery/IMG(38).jpg',
+           alt:""
+            },
+           {
+           src:'../../../public/gallery/IMG(39).jpg',
+           alt:""
+            }
+    ,] }]
+  ,
+  [{data:
+   [ 
+    {
+           src:'../../../public/gallery/IMG(40).jpg',
+           alt:""
+            },
+           {
+           src:'../../../public/gallery/IMG(41).jpg',
+           alt:""
+            },
+           {
+           src:'../../../public/gallery/IMG(42).jpg',
+           alt:""
+            }
+    ,] }]
+  ,
+  [{data:
+   [ 
+    {
+           src:'../../../public/gallery/IMG(4).jpg',
+           alt:""
+            },
+    ,] }]
+  ,
+
  
 ]
 
